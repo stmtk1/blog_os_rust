61197b8b9ed7abf6c47300e90977620e63764dc3
斎藤基樹 <stmtk13044032@gmail.com>
2020-10-20 10:12:13 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+// static HELLO: &[u8] = b"Hello World!";
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
+    loop {}
+}
